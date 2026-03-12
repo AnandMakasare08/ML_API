3c4c5fe3b072c6fea7d017ac5f7ba809ce555aef
Anand <makasareanand08@gmial.com>
2026-03-13 02:06:47 +0530

    done bhai

--- app.py
+++ app.py
@@ -77,5 +77,6 @@ def predict():
         "confidence":round(confidence,2)
     })
 
-if __name__=="__main__":
-    app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
